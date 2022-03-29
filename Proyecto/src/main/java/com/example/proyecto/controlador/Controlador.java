f0a40d1fbd74bb527a55f11d69f3324b63dc6037
Fernando <fernandondin@ciencias.unam.mx>
2022-03-29 02:47:08 -0600

    Cambiando fuente y redireccionando

---
+++ Proyecto/src/main/java/com/example/proyecto/controlador/Controlador.java
@@ -0,0 +1,30 @@
+package com.example.proyecto.controlador;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class Controlador {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/procesa")
+    public String procesa(HttpServletRequest request, Model model ) {
+        String titulo = request.getParameter("titulo");
+        String nota = request.getParameter("nota");
+        System.out.println("titulo -> " + titulo);
+        System.out.println("nota -> " + nota);
+        model.addAttribute("titulo", "Nombre");
+        model.addAttribute("nota", "Fernando Gerardo Flores García");
+        return "final";
+    }
+
+}
--- Proyecto/src/test/java/com/example/proyecto/ProyectoApplicationTests.java
+++ Proyecto/src/test/java/com/example/proyecto/ProyectoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Proyecto;
+package com.example.proyecto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
