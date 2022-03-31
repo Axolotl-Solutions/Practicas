0b73f9c2956f5b17639d055a0a3f22414fb96156
axelthepony27 <axelgarcia@ciencias.unam.mx>
2022-03-30 20:35:31 -0500

    Arregla los archivos (termina Practica 1)
    Este commit hace las Ãltimas modificaciones a los archivos
    Controlador.java, index.html y nombre.html para que se desplieguen todos
    los nombres en la ultima pagina. Aqui termina la implementacion de la
    Practica 1.

--- Proyecto/src/main/java/com/example/Proyecto/controlador/Controlador.java
+++ Proyecto/src/main/java/com/example/Proyecto/controlador/Controlador.java
@@ -18,10 +18,15 @@ public class Controlador {
     // http://localhost:8080/procesa
     @PostMapping("/procesa")
     public String procesa(HttpServletRequest request, Model model) {
-        String nombres = " Axel David García Beltrán\n Camila Alexandra Cruz Miranda\n Fernando Gerardo Flores García\n Ian Israel Garcia Vazquez";
-        String nombre = request.getParameter("nombre");
-        System.out.println("nombre -> " + nombre + "\n" + nombres);
-        model.addAttribute("nombre", nombre);
+        String nombres = "Axel David García Beltrán \n Camila Alexandra Cruz Miranda \n Fernando Gerardo Flores García \n Ian Israel Garcia Vazquez";
+        String nombre1 = request.getParameter("nombre1");
+        String nombre2 = request.getParameter("nombre2");
+        String nombre3 = request.getParameter("nombre4");
+        String nombre4 = request.getParameter("nombre3");
+        model.addAttribute("nombre1", nombre1);
+        model.addAttribute("nombre2", nombre2);
+        model.addAttribute("nombre3", nombre3);
+        model.addAttribute("nombre4", nombre4);
         return "nombre";
     }
 
